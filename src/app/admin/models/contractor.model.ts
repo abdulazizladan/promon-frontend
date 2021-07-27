67d1139661f78d3887a89557947a6e6dc7ad7b16
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-07-27 14:48:34 +0100

    task: Init contractor, project & User models

---
+++ src/app/admin/models/contractor.model.ts
@@ -0,0 +1,28 @@
+export interface Contractor{
+  _id: string,
+  name: string,
+  personnel: Array<Staff>
+}
+
+interface Staff{
+  _id: string,
+  first_name: string,
+  last_name: string,
+  role: string,
+  email: string,
+  phone_number: string
+}
+
+interface Branch{
+  _id: string,
+  name: string,
+  address: Address
+}
+
+interface Address{
+  _id: string,
+  street_address: string,
+  lga: string,
+  state: string,
+  country: string
+}
