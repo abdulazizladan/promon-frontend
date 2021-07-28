868d33496b22eea45cfb9ee2c23c2b00aef3048d
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-07-28 14:40:03 +0100

    task: Generate project, contractor and user components

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,10 +6,25 @@ import { StoreModule } from '@ngrx/store';
 import * as fromAdmin from './reducers';
 import { EffectsModule } from '@ngrx/effects';
 import { AdminEffects } from './admin.effects';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { ProjectsListComponent } from './components/projects-list/projects-list.component';
+import { ContractorsListComponent } from './components/contractors-list/contractors-list.component';
+import { AddProjectComponent } from './components/add-project/add-project.component';
+import { AddContractorComponent } from './components/add-contractor/add-contractor.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    UsersListComponent,
+    ProjectsListComponent,
+    ContractorsListComponent,
+    AddProjectComponent,
+    AddContractorComponent,
+    AddUserComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule,
