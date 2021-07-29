8136f836703a4a4ce9247645788ae8b3ef089ed3
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-07-29 15:28:20 +0100

    fix: Fix admin routes

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 //components
 import { AdminLayoutComponent } from './components/admin-layout/admin-layout.component';
+import { ContractorsListComponent } from './components/contractors-list/contractors-list.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProjectsListComponent } from './components/projects-list/projects-list.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
 const routes: Routes = [
   {
@@ -12,7 +15,27 @@ const routes: Routes = [
     children: [
       {
         path: '',
+        redirectTo: 'dashboard'
+      },
+      {
+        path: 'dashboard',
         component: DashboardComponent
+      },
+      {
+        path: 'projects',
+        component: ProjectsListComponent
+      },
+      {
+        path: 'contractors',
+        component: ContractorsListComponent
+      },
+      {
+        path: 'users',
+        component: UsersListComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'dashboard'
       }
     ]
   }
