ddaa0216a7514a8d7cf67fdc43903ef25a56a9ea
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-07-30 13:02:40 +0100

    task: Activate add-user dialog in user-list component

--- src/app/admin/components/users-list/users-list.component.ts
+++ src/app/admin/components/users-list/users-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddUserComponent } from '../add-user/add-user.component';
 
 @Component({
   selector: 'app-users-list',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersListComponent implements OnInit {
 
-  constructor() { }
+  constructor( public dialog: MatDialog ) { }
 
   ngOnInit(): void {
+  }
+
+  openDialog() {
+    this.dialog.open(AddUserComponent)
   }
 
 }
