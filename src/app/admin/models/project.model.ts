932dcfc2c2f96dd97c8f4b32c046bdc15c07d90a
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-08-15 14:00:02 +0100

    task: First build

--- src/app/admin/models/project.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Project{
-  _id: string,
-  title: string,
-  description: string,
-  location: Array<string>,
-  date_awarded: Date
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: '127.0.0.1:3000'
-};
