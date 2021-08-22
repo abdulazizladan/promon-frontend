b45569649c3c04c72c6cf50011399b0b5d752668
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-08-22 06:30:46 +0100

    task: Comment on functions

--- src/app/admin/components/add-project/add-project.component.ts
+++ src/app/admin/components/add-project/add-project.component.ts
@@ -10,11 +10,12 @@ import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 })
 export class AddProjectComponent implements OnInit {
 
+  error: string = '';
   public addProjectForm!: FormGroup;
   public country: any;
 
   /**
-   * 
+   * Dependencies
    * @param projectService 
    * @param fb 
    */
@@ -24,7 +25,7 @@ export class AddProjectComponent implements OnInit {
     ) { }
 
   /**
-   * 
+   * First lifecycle hook
    */
   ngOnInit(): void {
     this.getStates();
@@ -32,7 +33,7 @@ export class AddProjectComponent implements OnInit {
   }
 
   /**
-   * 
+   * Initialize form
    */
   initializeForm(): void{
     this.addProjectForm = this.fb.group({
@@ -42,12 +43,15 @@ export class AddProjectComponent implements OnInit {
     })
   }
 
+  /**
+   * get array of states and their LGAs
+   */
   getStates() {
     this.projectService.getStates().subscribe(
       res => {
         this.country = res;
       }, err => {
-
+        this.error = "Unable to get states";
       }
     )
   }
