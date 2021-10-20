a255f0755bab1e38e7f204dc7e7891b8b9f4f9dd
Abdulaziz Ladan <abdulazizaldan@gmail.com>
2021-10-20 17:05:00 +0100

    task Add material and flex-layout modules to projects module

--- src/app/features/projects/projects-routing.module.ts
+++ src/app/features/projects/projects-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from 'src/app/shared/components/not-found/not-found.component';
+import { ProjectSummaryComponent } from './components/project-summary/project-summary.component';
+import { ProjectsListComponent } from './components/projects-list/projects-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProjectsListComponent
+  },
+  {
+    path: 'project/:id',
+    component: ProjectSummaryComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/projects/projects.module.ts
+++ src/app/features/projects/projects.module.ts
@@ -1,5 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+
 
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsListComponent } from './components/projects-list/projects-list.component';
@@ -7,7 +17,7 @@ import { ProjectSummaryComponent } from './components/project-summary/project-su
 import { AddProjectComponent } from './components/add-project/add-project.component';
 import { StoreModule } from '@ngrx/store';
 import * as fromProjects from './reducers';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -17,6 +27,14 @@ import * as fromProjects from './reducers';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    SharedModule,
     ProjectsRoutingModule,
     StoreModule.forFeature(fromProjects.projectsFeatureKey, fromProjects.reducers, { metaReducers: fromProjects.metaReducers })
   ]
