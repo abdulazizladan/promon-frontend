f7ac8f4adaff6d3d2635082bccd2979de399a01b
Abdulaziz Ladan <abdulazizaldan@gmail.com>
2021-10-27 15:58:28 +0100

    feat: Init auth effects

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
-
+import { Actions, ofType, createEffect } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, exhaustMap, map } from 'rxjs/operators';
+import { signin, signout } from './auth.actions';
+import { User } from './models/User.model';
+import { AuthService } from './services/auth.service';
 
 
 @Injectable()
 export class AuthEffects {
 
-  constructor(private actions$: Actions) {
+  constructor
+  (private actions$: Actions,
+  private authService: AuthService) {
 
   }
+
+  //signin$ = createEffect(() => this.actions$.pipe(
+  //  ofType(signin),
+  //  exhaustMap(action =>
+  //    this.authService.signin(user).pipe(
+  //      map(user => signin({ user:  })),
+  //      catchError(error => of(AuthApiActions.loginFailure({ error })))
+  //    ))
+  //))
 
 }
