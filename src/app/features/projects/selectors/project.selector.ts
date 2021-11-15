81e7d7a0ab7235ecd1d4477d86cb2572e6003806
Abdulaziz Ladan <abdulazizaldan@gmail.com>
2021-11-15 15:17:38 +0100

    task: Add projects selector

---
+++ src/app/features/projects/selectors/project.selector.ts
@@ -0,0 +1,10 @@
+import { createSelector, createFeatureSelector } from "@ngrx/store";
+import { Project } from "../models/project.model";
+import { ProjectsState } from "../reducers";
+
+export const selectProjetcsState = createFeatureSelector<ProjectsState>('projects')
+
+const selectAllProjects = createSelector(
+  selectProjetcsState,
+  (state: ProjectsState) => state.projects
+)
