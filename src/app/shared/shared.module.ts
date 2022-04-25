6bce498977cdd6cdc6f77cddab8638c94ba6f463
Abdulaziz Ladan <abdulazizaldan@gmail.com>
2022-04-25 10:28:32 +0100

    task: Init shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { MessageComponent } from './components/message/message.component';
+import { LoaderService } from './services/loader.service';
+import { MessageService } from './services/message.service';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 
 
 @NgModule({
   declarations: [
-    NotFoundComponent
+    NotFoundComponent,
+    LoaderComponent,
+    MessageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatProgressBarModule
   ],
   exports: [
-    NotFoundComponent
+    NotFoundComponent,
+    LoaderComponent,
+    MessageComponent
+  ],
+  providers: [
+    LoaderService,
+    MessageService
   ]
 })
 export class SharedModule { }
